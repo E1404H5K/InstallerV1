d8260f1343327568f845bc5b802c8bd8ceb6c50b
E1404HSK <51128442+E1404H5K@users.noreply.github.com>
2019-06-12 20:34:53 +0700

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,155 @@
+import os
+import sys
+from datetime import datetime
+
+
+print("""
+\033[1;92m........___.......___.......
+.......|   |.....|   |.............
+.......|   |.....|   |..................
+.......|   |_____|   |.........................
+.......|    _____    |..............................
+.......|   |.....|   |..................................
+.......|   |.....|   |............ ......................
+.......|___|.....|___|.......................................
+================================........................
+********************************.....................
+\.....{ T00LS INSTALLER }......//..................
+.\....{ AUTHOR :E1404HSK}.....//...............
+..\......| L0WER CLASS |.....//...............
+...\........................//............
+....\......................//.........
+.....=======================.........
+""")
+
+sekarang = datetime.now()
+
+
+print(sekarang)
+print("\033[1;91m=------------=")
+print("   M E N U   ")
+print("=------------=")
+
+print("[1]ProgramSederhana\n[2]Recondog\n[3]SocialFish(Phishing Tool)\n[4]UsersRecon\n[5]DarkSploit\n[6]Hash Cracker\n[7]CMS map\n[8]TM-venom\n[9]DarkFly Tool\n[10]BlackEye")
+pilih = input("Masukan Pilihan Anda: ")
+if pilih == "1":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/E1404H5K/ProgramSederhana")
+	os.system("cd ProgramSederhana")
+	opsi1 = input("Exit??   Y/N: ")
+	if opsi1 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "2":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/UltimateHackers/Recondog")
+	os.system("cd Recondog")
+	opsi2 = input("Exit??   Y/N: ")
+	if opsi2 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "3":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/UndeadSec/SocialFish.git")
+	os.system("cd SocialFish")
+	opsi3 = input("Exit??   Y/N: ")
+	if opsi3 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "4":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/thelinuxchoice/userrecon")
+	os.system("cd userrecon")
+	opsi4 = input("Exit??   Y/N: ")
+	if opsi4 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "5":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/anthrax3/DarkSploit")
+	os.system("cd DarkSploit")
+	opsi5 = input("Exit??   Y/N: ")
+	if opsi5 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "6":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/ciku370/hasher")
+	os.system("cd hasher")
+	opsi6 = input("Exit??   Y/N: ")
+	if opsi6 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "7":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/Dionach/CMSmap.git")
+	os.system("cd CMSmap")
+	opsi7 = input("Exit??   Y/N: ")
+	if opsi7 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "8":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/TechnicalMujeeb/tmvenom")
+	os.system("cd tmvenom")
+	opsi8 = input("Exit??   Y/N: ")
+	if opsi8 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+elif pilih == "9":
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/Ranginang67/DarkFly-Tool")
+	os.system("cd DarkFly-Tool")
+	opsi9 = input("Exit??   Y/N: ")
+	if opsi9 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+else:
+	print("Mohon Tunggu.............................................................................................")
+	os.system("clear")
+	os.system("exit")
+	os.system("cd ..")
+	os.system("git clone https://github.com/thelinuxchoice/blackeye")
+	os.system("cd blackeye")
+	opsi10 = input("Exit??   Y/N: ")
+	if opsi10 == "Y":
+		os.system("exit")
+	else:
+		os.system("python tool.py")
+
